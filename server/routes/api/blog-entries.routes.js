c54fa50bb73e94e6adf83ae356705a57ed31ed8d
Jeffrey <renjeffreyy@gmail.com>
2020-06-23 19:53:18 -0700

    added route to get all blog posts from all users

--- server/routes/api/blog-entries.routes.js
+++ server/routes/api/blog-entries.routes.js
@@ -6,7 +6,7 @@ const auth = require('../../middleware/auth.check');
 const BlogEntries = require('../../models/blog-entries.model');
 const User = require('../../models/users.model');
 
-// @route    POST api/posts
+// @route    POST api/blog
 // @desc     Create a post
 // @access   Private
 router.post(
@@ -37,5 +37,18 @@ router.post(
     }
   }
 );
+
+// @route    GET api/blog
+// @desc     Get all posts
+// @access   public
+router.get('/', async (req, res) => {
+  try {
+    const posts = await BlogEntries.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
